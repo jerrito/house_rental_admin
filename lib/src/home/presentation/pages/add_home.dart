2d6b4706ec9be2abbc373c65136e530c0c81c362
ShaQ Express <shaqexpress@ShaQ-mac-Papa-Kay.local>
2024-01-10 09:46:09 +0000

    addHome page created

---
+++ lib/src/home/presentation/pages/add_home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:house_rental_admin/src/authentication/presentation/widgets/default_textfield.dart';
+
+class AddHomePage extends StatefulWidget {
+  const AddHomePage({super.key});
+
+  @override
+  State<AddHomePage> createState() => _AddHomePageState();
+}
+
+class _AddHomePageState extends State<AddHomePage> {
+  final homeNameController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add Home or Room"),
+      ),
+      body: Column(
+        children: [
+
+          DefaultTextfield(
+            controller: homeNameController,
+            hintText: "Enter home name",
+            label: "Home name",
+
+          ),
+
+          DefaultTextfield(
+            controller: homeNameController,
+            hintText: "Enter rent amount",
+            label: "Rent amount",
+          ),
+
+           DefaultTextfield(
+            controller: homeNameController,
+            hintText: "Enter home description",
+            label: "Home Description",
+          ),
+
+          
+
+        ],
+      ),
+    );
+  }
+}
